9fa103969133b5f544cd01dc4edd587c783ecc95
VictorTaborda1 <vtaborda@unipacifico.edu.co>
2026-03-24 12:22:42 -0500

    un avance muy bueno

--- parcial1/nivelbasico.c
+++ parcial1/nivelbasico.c
@@ -1,12 +1,13 @@
 #include<stdio.h>
 #define N  7
 void bubbleSort(int arr[], int n);
+void selectionSort(int arr[], int n);
+void insertionSort(int arr[], int n) ;
 void mostrar();
 int arreglo[N];
 int main(){
     int opcion;
     printf("vamos a comenzar");
-    bubbleSort(arreglo, 7);
     do{
         printf("bienvenido al sistema de ordenamiento de numeros ENTEROS");
         printf("seleciona la accion a realizar\n");
@@ -17,9 +18,20 @@ int main(){
         printf("5. salir \n");
         scanf("%d",&opcion);
         switch (opcion){
-        case 1:
-            break;
-        if( arreglo != NULL):
+            case 1: for (int i = 0; i < N; i++) {
+                    printf("Ingrese el numero %d: ", i + 1);
+                    scanf("%d", &arreglo[i]);
+                    }
+                    // Mostrar los datos
+                    printf("\nLos numeros ingresados son:\n");
+                    mostrar();break;
+            case 2:
+                    bubbleSort(arreglo, N);break;
+            case 3:
+                    selectionSort(arreglo,7);break;
+            case 4:
+                    insertionSort(arreglo,7);break;
+
         };
     } while (opcion!=5) ;
     
@@ -39,7 +51,37 @@ void bubbleSort(int arr[], int n) {
         } 
     } 
 }
-
+void selectionSort(int arr[], int n) { 
+    for (int i = 0; i < n - 1; i++) { 
+        mostrar();
+        int minIndex = i; 
+        // Encontrar el mínimo en arr[i..n-1] 
+        for (int j = i + 1; j < n; j++) { 
+            if (arr[j] < arr[minIndex]) { 
+                minIndex = j; 
+            } 
+        } 
+        // Intercambiar (swap) el mínimo encontrado con el elemento en i 
+        int temp = arr[i]; 
+        arr[i] = arr[minIndex]; 
+        arr[minIndex] = temp; 
+    } 
+    mostrar();
+}
+void insertionSort(int arr[], int n) {
+    for (int i = 1; i < n; i++) {
+        int clave = arr[i]; // Elemento a insertar
+        int j = i - 1;
+        // Desplazar hacia la derecha los elementos mayores que 'clave'
+        while (j >= 0 && arr[j] > clave) {
+            arr[j + 1] = arr[j];
+            j--;
+        }
+        // Insertar 'clave' en la posición que dejó libre
+        arr[j + 1] = clave;
+        mostrar();
+    }
+}
 void mostrar(){
     for(int i=0; i<N; i++){
         printf("%d - ", arreglo[i]);
