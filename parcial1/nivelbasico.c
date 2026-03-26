53a815a2ceea538625603723834bebe47d4625e3
VictorTaborda1 <vtaborda@unipacifico.edu.co>
2026-03-25 19:08:07 -0500

    funciona pero cambiar algo no aria daño

--- parcial1/nivelbasico.c
+++ parcial1/nivelbasico.c
@@ -1,30 +1,36 @@
 #include<stdio.h>
-#define N  7
+#define Nmax  100
+int n=0 ;
+int arreglo[Nmax]; 
+int datosIngresados = 0;
 void bubbleSort(int arr[], int n);
 void selectionSort(int arr[], int n);
 void insertionSort(int arr[], int n) ;
 void mostrar();
-int arreglo[N];
 int main(){
     int opcion;
-    printf("vamos a comenzar");
+    printf("vamos a comenzar\n");
     do{
-        printf("bienvenido al sistema de ordenamiento de numeros ENTEROS");
+        printf("bienvenido al sistema de ordenamiento de numeros ENTEROS\n");
         printf("seleciona la accion a realizar\n");
-        printf("1. agregar numeros ENTEROS a la lista a ordenar maximo 7 numeros\n");
+        printf("1. agregar numeros ENTEROS a la lista a ordenar\n");
         printf("2. Ordenar segun el metodo BubbleSort\n");
         printf("3. Ordenar segun el metodo SelectionSort\n");
         printf("4. Ordenar segun el metodo insertSort\n");
         printf("5. salir \n");
         scanf("%d",&opcion);
         switch (opcion){
-            case 1: for (int i = 0; i < N; i++) {
+            case 1: printf("ingresa el numero maximo de numeros que quieres ingresar");
+                    scanf("%d",&n);
+                    #define N n
+                 for (int i = 0; i < N; i++) {
                     printf("Ingrese el numero %d: ", i + 1);
                     scanf("%d", &arreglo[i]);
                     }
                     // Mostrar los datos
                     printf("\nLos numeros ingresados son:\n");
-                    mostrar();break;
+                    mostrar();
+                    datosIngresados= 1; break;
             case 2:
                     bubbleSort(arreglo, N);break;
             case 3:
@@ -39,6 +45,10 @@ int main(){
 return 0;
 }
 void bubbleSort(int arr[], int n) { 
+    if (!datosIngresados){
+        printf("primero registra datos\n");
+        return;
+    }
     for (int i = 0; i < n - 1; i++) { 
         for (int j = 0; j < n - i - 1; j++) { 
             mostrar();
@@ -52,6 +62,11 @@ void bubbleSort(int arr[], int n) {
     } 
 }
 void selectionSort(int arr[], int n) { 
+    if (!datosIngresados){
+        printf("primero registra datos\n");
+        return;
+    }
+    mostrar();
     for (int i = 0; i < n - 1; i++) { 
         mostrar();
         int minIndex = i; 
@@ -69,6 +84,11 @@ void selectionSort(int arr[], int n) {
     mostrar();
 }
 void insertionSort(int arr[], int n) {
+    if (!datosIngresados){
+        printf("primero registra datos\n");
+        return;
+    }
+    mostrar();
     for (int i = 1; i < n; i++) {
         int clave = arr[i]; // Elemento a insertar
         int j = i - 1;
